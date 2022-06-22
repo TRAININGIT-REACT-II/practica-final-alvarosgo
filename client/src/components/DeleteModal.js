869984cffd17e1761e8be986dfe6ef9856a9bb22
Alvaro Sanchez <alvarosanchezgonzalez10@gmail.com>
2022-06-22 17:00:07 +0200

    Cambio en los className del modal

--- client/src/components/DeleteModal.js
+++ client/src/components/DeleteModal.js
@@ -31,19 +31,19 @@ const DeleteModal = ({ show, onClose }) => {
 
     if (show && modalRef.current != null) {
         return createPortal(
-            <div class="modal fade in" id="deleteModal" aria-labelledby="exampleModalLabel" aria-hidden="true" onClick={onClose}>
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade in" id="deleteModal" aria-labelledby="exampleModalLabel" aria-hidden="true" onClick={onClose}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             ...
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
